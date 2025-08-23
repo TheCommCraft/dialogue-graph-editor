ccdf96c017619423a84073b43cf75c19c2f84564
TheCommCraft <simon.c.gilde@gmail.com>
2025-08-23 21:08:47 +0200

    changed to params

--- src/jsMain/kotlin/de/thecommcraft/dialoguegrapheditor/FileStorageSession.kt
+++ src/jsMain/kotlin/de/thecommcraft/dialoguegrapheditor/FileStorageSession.kt
@@ -7,6 +7,7 @@ import web.http.*
 import web.streams.ReadableStreamReadResult
 import web.streams.read
 import web.url.URL
+import web.url.URLSearchParams
 
 external class FileContent {
     var content: String
@@ -28,7 +29,7 @@ class FileStorageSession(
     suspend fun write(data: String) {
         if (data == previousData) return
         previousData = data
-        fetch(serverBaseUrl / "files" / fileTarget, RequestInit(
+        fetch(serverBaseUrl / "files" / fileTarget params makeParams(), RequestInit(
             method = RequestMethod.PUT,
             body = BodyInit(JSON.stringify(unsafeJso<FileContent> {
                 content = data
@@ -38,7 +39,7 @@ class FileStorageSession(
     }
 
     suspend fun read(data: String): String {
-        val response = fetch(serverBaseUrl / "files" / fileTarget, RequestInit(
+        val response = fetch(serverBaseUrl / "files" / fileTarget params makeParams(), RequestInit(
             method = RequestMethod.GET,
             headers = makeHeaders()
         ))
@@ -48,26 +49,28 @@ class FileStorageSession(
     }
 
     suspend fun forcePush() {
-        fetch(serverBaseUrl / "save_data/", RequestInit(
+        fetch(serverBaseUrl / "save_data/" params makeParams(), RequestInit(
             method = RequestMethod.POST,
             headers = makeHeaders(),
         ))
     }
 
+    private fun makeParams(): String {
+        val params = URLSearchParams()
+        params.append("data_access_token", dataAccessToken)
+        manualUpdateToken?.let {
+            params.append("manual_update_token", manualUpdateToken)
+        }
+        return params.toString()
+    }
+
     private fun makeHeaders(): Headers {
         val headers = Headers()
         headers.append("Content-Type", "application/json")
-        headers.append("TestCookie", "CookieValue")
-        headers.append("TestCookie2", "")
-        headers.append("Cookie", JSON.stringify(unsafeJso<RawCredentials> {
-            data_access_token = dataAccessToken
-            manualUpdateToken?.let {
-                manual_update_token = it
-            }
-        }))
-        console.log(headers)
         return headers
     }
 }
+
+infix fun URL.params(params: String) = URL(params, this)
 
 operator fun URL.div(endPoint: String) = URL(encodeURIComponent(endPoint.slice(0..<endPoint.lastIndex ))+endPoint.last(), this.toString().removeSuffix("/")+"/")
